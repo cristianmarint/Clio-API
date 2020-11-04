6f5f12bf4b18945d1647729a1ac8eb9d0b974043
Cristian Marín <cristianmarint@gmail.com>
2020-11-04 11:19:57 -0500

    Commit inicial

---
+++ src/test/java/com/biblioteca/demeter/BibliotecaDemeterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.biblioteca.demeter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaDemeterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
