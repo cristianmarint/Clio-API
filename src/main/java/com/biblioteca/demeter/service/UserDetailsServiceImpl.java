8f3becc3ec842c5a9894beed755c972f0933db6f
Cristian Marin <cristianmarint@gmail.com>
2020-12-09 17:01:21 -0500

    Fix compiler errors :D

--- src/main/java/com/biblioteca/demeter/service/UserDetailsServiceImpl.java
+++ src/main/java/com/biblioteca/demeter/service/UserDetailsServiceImpl.java
@@ -37,7 +37,7 @@ public class UserDetailsServiceImpl implements UserDetailsService {
         return new org.springframework.security.core.userdetails.User(
                 user.getUsername(),
                 user.getPassword(),
-                user.isEnable(),
+                user.isEnabled(),
                 true,
                 true,
                 true,
